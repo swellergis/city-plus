957aa68d0d194fb1007000bacc1605e2ff6cda40
swellergis <shaun.weller@gmail.com>
2023-09-13 13:47:24 -0600

    minor change

--- src/main/java/com/sp/cityplus/controller/HomeController.java
+++ src/main/java/com/sp/cityplus/controller/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping
     public String sayHello(){
-        return "Howdy doody, World";
+        return "Howdy doody, Yo";
     }
 }
