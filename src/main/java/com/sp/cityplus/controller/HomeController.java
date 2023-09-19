78560e5f282d88757ca9bb97307b28fb0fccbc8d
swellergis <shaun.weller@gmail.com>
2023-09-19 13:05:42 +0000

    yo ho

--- src/main/java/com/sp/cityplus/controller/HomeController.java
+++ src/main/java/com/sp/cityplus/controller/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping
     public String sayHello(){
-        return "salty dog";
+        return "salty dog yo ho";
     }
 }
