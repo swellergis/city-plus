0519a174ddf017f8e08babb5257891ccfaf9c91b
swellergis <shaun.weller@gmail.com>
2023-09-25 14:52:46 -0600

    russia take two

--- src/main/java/com/sp/cityplus/controller/HomeController.java
+++ src/main/java/com/sp/cityplus/controller/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping
     public String sayHello(){
-        return "india";
+        return "russia with love";
     }
 }
