4dfe51ab029c2b4df79679ee6ae12a0b28a94a0c
swellergis <shaun.weller@gmail.com>
2023-09-26 15:08:42 +0000

    city

--- src/main/java/com/sp/cityplus/controller/HomeController.java
+++ src/main/java/com/sp/cityplus/controller/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping
     public String sayHello(){
-        return "shutdown!";
+        return "shutdown city!";
     }
 }
